cacf1acfe96f86665736bd7a38f5a87e99da020f
Adriel Alejandro Aliaga Benavides <aaliagab85@gmail.com>
2022-04-13 21:29:18 -0400

    lista de clientes con json externo

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,16 @@
+import {Cliente} from './cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id:1, nombre:'Jose',apellido:'Cold',createAt:'13/04/2022',email:'j.cold@gmail.com'},
+  {id:2, nombre:'Pedro',apellido:'Acosta',createAt:'13/04/2022',email:'pedrito@gmail.com'},
+  {id:3, nombre:'Yudisel',apellido:'Santana P',createAt:'13/04/2022',email:'yudisel@gmail.com'},
+  {id:4, nombre:'Jose2',apellido:'Cold',createAt:'13/04/2022',email:'j.cold@gmail.com'},
+  {id:5, nombre:'Pedro2',apellido:'Acosta',createAt:'13/04/2022',email:'pedrito@gmail.com'},
+  {id:6, nombre:'Yudisel2',apellido:'Santana P',createAt:'13/04/2022',email:'yudisel@gmail.com'},
+  {id:7, nombre:'Jose3',apellido:'Cold',createAt:'13/04/2022',email:'j.cold@gmail.com'},
+  {id:8, nombre:'Pedro3',apellido:'Acosta',createAt:'13/04/2022',email:'pedrito@gmail.com'},
+  {id:9, nombre:'Yudisel3',apellido:'Santana P',createAt:'13/04/2022',email:'yudisel@gmail.com'},
+  {id:10, nombre:'Jose4',apellido:'Cold',createAt:'13/04/2022',email:'j.cold@gmail.com'},
+  {id:11, nombre:'Pedro4',apellido:'Acosta',createAt:'13/04/2022',email:'pedrito@gmail.com'},
+  {id:12, nombre:'Yudisel4',apellido:'Santana P',createAt:'13/04/2022',email:'yudisel@gmail.com'}
+]
