086a5141faeedb0f0da8a51205a4625639ab01ce
Adriel Alejandro Aliaga Benavides <aaliagab85@gmail.com>
2022-04-22 20:58:22 -0400

    consumiendo api de SpringBoot con HttpClient

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
 import {Cliente} from './cliente';
-import {CLIENTES} from './clientes.json';
-import {of, Observable} from 'rxjs';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
-
-  constructor() { }
+  private urlEndPoint:string = 'http://localhost:8080/api/clientes';
+  constructor(private http: HttpClient) { }
 
 getClientes(): Observable<Cliente[]>{
-    return of(CLIENTES);
+    return this.http.get<Cliente[]>(this.urlEndPoint);
   }
 
 }
